eb6d7f86da88ad0e25e030aa5883696a50ead706
Ezra Stein <ezs4@cornell.edu>
2018-10-14 18:38:38 -0400

    Fixed opengl on mac

--- debug/src/app.cpp
+++ debug/src/app.cpp
@@ -11,15 +11,24 @@
 namespace dynsolver {
 namespace debug {
 
+namespace {
+void process_gl_errors() {
+  GLenum err;
+  while((err = glGetError()) != GL_NO_ERROR) {
+    printf("Found GL ERROR: 0x%x\n", err);
+  }
+}
+}
+
 bool app::OnInit() {
   frame = new wxFrame(nullptr, wxID_ANY, "My Frame",
-                      wxPoint(100, 100), wxSize(400, 400));
+                      wxPoint(100, 100), wxSize(800, 600));
 
   wxGLAttributes glAttrs;
-  glAttrs.Defaults().EndList();
-  
+  glAttrs.PlatformDefaults().DoubleBuffer().EndList();
+
   canvas = new wxGLCanvas(frame, glAttrs);
-  
+
   wxGLContextAttrs glContextAttrs;
   glContextAttrs
       .PlatformDefaults()
@@ -29,12 +38,12 @@ bool app::OnInit() {
   std::cout << "Running under opengl version: " <<
       std::to_string(GL_VERSION_MAJOR) << "."
             << std::to_string(GL_VERSION_MINOR) << std::endl;
-  
+
   glContext = new wxGLContext(canvas, NULL, &glContextAttrs);
   assert(glContext->IsOK());
 
   frame->Show(true);
-  
+
   bool success = glContext->SetCurrent(*canvas);
   assert(success);
 
@@ -43,15 +52,18 @@ bool app::OnInit() {
 
   auto onPaint =
       [&](wxPaintEvent& evt){
+        glContext->SetCurrent(*canvas);
         // Call some gl functions
-        glViewport(0, 0, 400, 400);
+        glViewport(0, 0, canvas->GetSize().GetWidth(),
+            canvas->GetSize().GetHeight());
         glClearColor(1.0f, 1.0f, 0.0, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         bool success = canvas->SwapBuffers();
         assert(success);
+        process_gl_errors();
       };
   canvas->Bind(wxEVT_PAINT, onPaint);
-  
+
   return true;
 }
 
