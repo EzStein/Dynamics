e0cb263d7ab690de4ee32f522eba7a5655f74a1c
Ezra Stein <ezstein10@gmail.com>
2018-11-03 15:04:04 -0400

    Set debugging for expr

--- debug/src/main.cpp
+++ debug/src/main.cpp
@@ -1,5 +1,25 @@
-#include <wx/wx.h>
+//#include <wx/wx.h>
 
-#include "debug/app.h"
+//#include "debug/app.h"
 
-wxIMPLEMENT_APP(dynsolver::debug::app);
+//wxIMPLEMENT_APP(dynsolver::debug::app);
+
+#include <iostream>
+#include "compiler/expression_parser.h"
+#include <climits>
+
+int main(void) {
+  dynsolver::compiler::expression_parser parse;
+  std::list<symbol> symbolTable;
+  symbolTable.push_back(symbol("x", 0, 0));
+  std::cout << "Expression:" << std::endl;
+  std::string expr;
+  std::getline(std::cin, expr);
+  std::cout << "Value:" << std::endl;
+  double val;
+  std::cin >> val;
+  AST ast = parse.parse(expr, symbolTable);
+  dynsolver::compiler::function<double, const double*> function = ast.compile();
+    std::cout << "Result: " << function(&val) << std::endl;
+  return 0;
+}
